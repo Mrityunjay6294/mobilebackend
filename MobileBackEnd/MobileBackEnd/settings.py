41852e5cd68f098f69cb3b00b9c5245098810ce5
mrityunjay <mrityunjay.xam@gmail.com>
2021-05-27 16:55:55 +0530

    backend project 2

--- MobileBackEnd/MobileBackEnd/settings.py
+++ MobileBackEnd/MobileBackEnd/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-%px!gf(x7e7u#9(g7t4y+7fj=n)-**tsf4o3wo1!c$njxk!%cs
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -126,6 +126,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
